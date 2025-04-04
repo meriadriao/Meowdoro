92e2a69d7dc95b16a5c2e9356f805cf4554c414c
Meri Adrião <109561091+meriadriao@users.noreply.github.com>
2025-04-04 17:05:22 -0300

    added new assets and updated README. updated image command.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Meowdoro
-Simple pomodoro + to-do list app.
+# MEOWDORO
+Simple pomodoro app.
 Project is being created and updated as I, Meri, learn more about front-end development.
+
+## SOON
+- To-do list
+
+## CODE CREDITS (thank you for your help!)
+- [W3Schools](https://www.w3schools.com/howto/howto_js_todolist.asp)
+- [How to Web Dev](https://youtu.be/sVSAklXy1uE?si=XoJgkSsMVr7D4E9S)
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const pauseButton = document.getElementById("pauseButton");
 const resetButton = document.getElementById("resetButton");
 const timer = document.getElementById("timer");
 const timesLooped = document.getElementById("timesLooped");
+const cat1 = document.querySelector(".cat1");
+const cat2 = document.querySelector(".cat2");
 
 let timeLeft = 1500; // 25 minutes in seconds
 let Interval = null;
@@ -12,15 +14,29 @@ const updateTimer = () => {
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
   timer.innerHTML = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  updateCatsVisibility();
 };
 
 const updateTimesLooped = () => {
   timesLooped.innerHTML = `${completedCount}/4 completed`;
+  updateCatsVisibility();
+}
+
+function updateCatsVisibility() {
+  if (timeLeft === 1500) {
+    cat2.classList.add("hidden");
+    cat1.classList.remove("hidden");
+  }
+  if (timeLeft < 1500 && timeLeft > 0) {
+    cat2.classList.remove("hidden");
+    cat1.classList.add("hidden");
+  }
 }
 
 const startTimer = () => {
   if (Interval === null) {
     console.log("Starting timer...");
+    updateCatsVisibility();
     Interval = setInterval(() => {
       timeLeft--;
       updateTimer();
@@ -50,10 +66,12 @@ const resetTimer = () => {
   Interval = null;
   timeLeft = 1500;
   updateTimer();
+  updateCatsVisibility();
 };
 
 startButton.addEventListener("click", startTimer);
 pauseButton.addEventListener("click", stopTimer);
 resetButton.addEventListener("click", resetTimer);
 
+updateCatsVisibility();
 updateTimesLooped();
